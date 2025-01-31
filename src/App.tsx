35026d9f3202aa8a99159d9978354f9ec6645587
Seungmani <dkelcncjs@naver.com>
2025-01-31 20:50:44 +0900

    [conf] 초반 환경 설정

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="">일반: MinSans Regular 폰트입니다.</p>
+      <p className="font-minSans font-normal">
+        이것은 MinSans Regular 폰트입니다.
+      </p>
+      <p className="font-minSans font-medium">
+        이것은 MinSans Medium 폰트입니다.
+      </p>
+      <p className="font-minSans font-semibold">
+        이것은 MinSans SemiBold 폰트입니다.
+      </p>
+      <p className="font-minSans font-bold">이것은 MinSans Bold 폰트입니다.</p>
+      <p className="font-bold">일반 MinSans Bold 폰트입니다.</p>
+    </>
+  );
+}
+
+export default App;
