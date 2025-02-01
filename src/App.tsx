8938933a203d6e1a5e0edcc6880dd4be0f4305f3
choycoy <passionfruits479@gmail.com>
2025-02-01 17:28:32 +0900

    config: svg 컴포넌트화 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
-import "./App.css";
+import './App.css';
+import HomeIcon from './assets/icons/home.svg';
 
 function App() {
   return (
     <>
       <p className="">일반: MinSans Regular 폰트입니다.</p>
-      <p className="font-minSans font-normal">
-        이것은 MinSans Regular 폰트입니다.
-      </p>
-      <p className="font-minSans font-medium">
-        이것은 MinSans Medium 폰트입니다.
-      </p>
-      <p className="font-minSans font-semibold">
-        이것은 MinSans SemiBold 폰트입니다.
-      </p>
+      <p className="font-minSans font-normal">이것은 MinSans Regular 폰트입니다.</p>
+      <p className="font-minSans font-medium">이것은 MinSans Medium 폰트입니다.</p>
+      <p className="font-minSans font-semibold">이것은 MinSans SemiBold 폰트입니다.</p>
       <p className="font-minSans font-bold">이것은 MinSans Bold 폰트입니다.</p>
       <p className="font-bold">일반 MinSans Bold 폰트입니다.</p>
+      <HomeIcon className="text-red-300" />
     </>
   );
 }
