41b237288449b54e38489b4f8745d99127ba9c9b
Seungmani <dkelcncjs@naver.com>
2025-02-02 20:56:50 +0900

    [feat] 메인 페이니(스터디 표시 페이지)  레이아웃 완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-import HomeIcon from '@/assets/icons/home.svg';
+import Router from './routes/routes';
 
 function App() {
   return (
     <>
-      <p className="">일반: MinSans Regular 폰트입니다.</p>
-      <p className="font-minSans font-normal">이것은 MinSans Regular 폰트입니다.</p>
-      <p className="font-minSans font-medium">이것은 MinSans Medium 폰트입니다.</p>
-      <p className="font-minSans font-semibold">이것은 MinSans SemiBold 폰트입니다.</p>
-      <p className="font-minSans font-bold">이것은 MinSans Bold 폰트입니다.</p>
-      <p className="font-bold">일반 MinSans Bold 폰트입니다.</p>
-      <HomeIcon className="text-main" />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,98 @@
 import React from 'react';
+import Logo from '../assets/logo.png';
+import Bell from '@/assets/icons/bell.svg';
+import HomeIcon from '@/assets/icons/home.svg';
+import { Link } from 'react-router-dom';
 
 const Home = React.memo((): JSX.Element => {
-  return <div>Home Page</div>;
+  return (
+    <div className="h-[100vh]">
+      <div className="w-full px-4">
+        {/* Header */}
+        <div className="w-[343px] h-[50px] flex items-center justify-between">
+          <img src={Logo} alt="Logo" className="w-[83px] h-[33px]" />
+          <Bell />
+        </div>
+
+        {/* 검색 창 */}
+        <Link to={'#'}>
+          <input
+            className="w-[343px] h-[23px] mb-3 border rounded-[10px] border-main text-black text-sm placeholder:pl-2"
+            type="text"
+            placeholder="검색어를 입력하세요."
+          />
+        </Link>
+
+        {/* 필터 */}
+        <div className="w-[343px] h-[24px] mb-3 flex items-center gap-[5px] overflow-y-auto scrollbar-hide">
+          <div className="px-[8px] py-[3px] rounded-[10px] text-sm bg-white-gray flex-shrink-0 whitespace-nowrap min-w-max">
+            # 코딩 x
+          </div>
+          <div className="px-[8px] py-[5px] rounded-[10px] text-sm bg-white-gray flex-shrink-0 whitespace-nowrap min-w-max">
+            # 10 ~ 40시간 x
+          </div>
+          <div className="px-[8px] py-[5px] rounded-[10px] text-sm bg-white-gray flex-shrink-0 whitespace-nowrap min-w-max">
+            # 10 ~ 40시간 x
+          </div>
+          <div className="px-[8px] py-[5px] rounded-[10px] text-sm bg-white-gray flex-shrink-0 whitespace-nowrap min-w-max">
+            # 10 ~ 40시간 x
+          </div>
+          <div className="px-[8px] py-[5px] rounded-[10px] text-sm bg-white-gray flex-shrink-0 whitespace-nowrap min-w-max">
+            # 10 ~ 40시간 x
+          </div>
+          <div className="px-[8px] py-[5px] rounded-[10px] text-sm bg-white-gray flex-shrink-0 whitespace-nowrap min-w-max">
+            # 10 ~ 40시간 x
+          </div>
+        </div>
+
+        {/* 생성하기 버튼 - 로그인 시 보여줌 없으면 공백*/}
+        <Link
+          to={'/study/create'}
+          className=" h-[30px] mb-3 float-right px-3 py-1 bg-main text-white text-center rounded-[10px]"
+        >
+          <span className="text-sm">+ 스터디 만들기</span>
+        </Link>
+
+        {/* 스터디 목록 */}
+        <div className="w-[343px] h-[400px] mb-3 flex flex-col items-center gap-[5px] overflow-y-scroll scrollbar-hide">
+          {/* 없으면 -> 등록된 스터디가 없습니다. */}
+          {Array.from({ length: 5 }).map((_, index) => (
+            <div key={index} className="w-full bg-white p-3 rounded-[10px] border border-white-gray">
+              <div className="mb-2">
+                <p className="font-bold">스터디 제목</p>
+                <p className="text-sm text-gray">스터디원 10명</p>
+              </div>
+              <div className="text-sm text-gray">
+                <p>스터디 기간: 2025-01-01 ~ 2025-02-01 (4주)</p>
+                <p>주당 목표 시간: 40시간</p>
+                <p>참여 포인트: 500</p>
+              </div>
+              <div className="flex gap-2 mt-2">
+                <p className="text-xs bg-white-gray px-2 py-1 rounded-md"># 태그</p>
+                <p className="text-xs bg-white-gray px-2 py-1 rounded-md"># 태그</p>
+                <p className="text-xs bg-white-gray px-2 py-1 rounded-md"># 태그</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* nav */}
+      <div className="w-full h-[55px] fixed bottom-0 left-0 flex items-center justify-around bg-white">
+        <Link to={'/'} className="flex flex-col items-center justify-center text-main">
+          <HomeIcon />
+          <p>홈</p>
+        </Link>
+        <Link to={'/'} className="flex flex-col items-center justify-center text-center text-gray">
+          <HomeIcon />
+          <p>그룹</p>
+        </Link>
+        <Link to={'/'} className="flex flex-col items-center justify-center text-center text-gray">
+          <HomeIcon />
+          <p>마이</p>
+        </Link>
+      </div>
+    </div>
+  );
 });
 
 export default Home;
