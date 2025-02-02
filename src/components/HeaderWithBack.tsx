298dd1ce47c53b7319862cd4c9aeec81e694eb88
choycoy <passionfruits479@gmail.com>
2025-02-02 21:23:16 +0900

    feat: 헤더 뒤로가기 버튼에 hover 적용

--- src/components/HeaderWithBack.tsx
+++ src/components/HeaderWithBack.tsx
@@ -15,20 +15,21 @@ export default function HeaderWithBack({
   };
 }) {
   const navigate = useNavigate();
-  const handleGoBack = () => {
-    navigate(-1);
-  };
 
   return (
     <header
       className={`fixed z-[999] top-0 flex items-center w-full h-[52px] pl-2 pr-4 ${isStudyPage ? 'bg-main text-white' : 'text-main border-b border-b-main'}`}
     >
-      <button onClick={handleGoBack} aria-label="뒤로 가기">
-        <ChevronLeftIcon alt="뒤로 가기" className="cursor-pointer" />
+      <button onClick={() => navigate(-1)} aria-label="뒤로 가기">
+        <ChevronLeftIcon alt="뒤로 가기" className="cursor-pointer transition-colors hover:text-white-gray" />
       </button>
       <h1 className="absolute left-1/2 -translate-x-1/2 font-bold">{title}</h1>
       {action && (
-        <button onClick={action.onClick} aria-label={action.ariaLabel} className="ml-auto">
+        <button
+          onClick={action.onClick}
+          aria-label={action.ariaLabel}
+          className="ml-auto hover:text-white-gray transition-colors"
+        >
           <action.icon alt={action.ariaLabel} />
         </button>
       )}
