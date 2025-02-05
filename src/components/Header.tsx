e726682966d41c02c9802e8d9a2971e4b3b21683
choycoy <passionfruits479@gmail.com>
2025-02-06 00:45:36 +0900

    design: header, fnb 768px이상에서도 중앙 정렬 및 border-x 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Bell from '@/assets/icons/bell.svg';
 
 export default function Header() {
   return (
-    <header className="md:border-white-gray fixed top-0 z-[999] mx-auto flex h-13 w-full max-w-3xl items-center justify-between bg-white px-4 md:border-x">
+    <header className="md:border-white-gray fixed top-0 left-1/2 z-[999] mx-auto flex h-13 w-full max-w-3xl -translate-x-1/2 items-center justify-between bg-white px-4 md:border-x">
       <Link to={'/'} aria-label="홈으로">
         <img src={Logo} alt="Logo" className="h-[30px] w-20" />
       </Link>
