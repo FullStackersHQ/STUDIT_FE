e30c840ea4e61e49133bc7e48c668468449d0c3f
choycoy <passionfruits479@gmail.com>
2025-02-06 23:25:59 +0900

    design: 스터디 목록 페이지 padding bottom 추가

--- src/pages/StudyList.tsx
+++ src/pages/StudyList.tsx
@@ -9,7 +9,7 @@ export default function StudyList() {
   return (
     <div>
       <HeaderWithBack title={title} />
-      <section className="flex flex-col gap-y-2 px-4 pt-3">
+      <section className="flex flex-col gap-y-2 px-4 pt-3 pb-7">
         {studyList.map((studyItem, index) => (
           <StudyItem study={studyItem} studyType={studyType} key={index} />
         ))}
