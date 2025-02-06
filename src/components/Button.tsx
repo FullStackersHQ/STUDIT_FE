a87402712b42036d386c3ef3f17cb72939907520
choycoy <passionfruits479@gmail.com>
2025-02-06 14:43:43 +0900

    design: 공용 컴포넌트 Button 작은 사이즈 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,15 +3,17 @@ export default function Button({
   onClick,
   ariaLabel,
   extraClass,
+  isSmall,
 }: {
   text: string;
   onClick: () => void;
   ariaLabel?: string;
   extraClass?: string;
+  isSmall?: boolean;
 }) {
   return (
     <button
-      className={`bg-sub text-main hover:bg-sub-hover hover:text-main-hover h-12 w-full rounded-full text-sm font-bold transition-colors ${extraClass}`}
+      className={`bg-sub text-main hover:bg-sub-hover hover:text-main-hover h-12 rounded-full text-sm font-bold transition-colors ${extraClass} ${isSmall ? 'w-[320px]' : 'w-full'}`}
       aria-label={ariaLabel}
       onClick={onClick}
     >
