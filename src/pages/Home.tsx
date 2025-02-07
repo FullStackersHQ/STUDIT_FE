c08b6080bf9897808d282cf4018af23e0d6f9f6e
Seungmani <dkelcncjs@naver.com>
2025-02-07 23:13:03 +0900

    [refactor] 메인 페이지 dummy 데이터 추가 및 UI 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,18 +8,26 @@ export default function Home(): JSX.Element {
   const isLogin = true;
 
   return (
-      <div className="h-[100vh-70px] px-4">
+    <div className="px-4">
+      <div style={{ height: `calc(100vh - 52px - 48px - 50px)` }}>
         <Link to={'/search'}>
-          <input className="search-input" type="text" placeholder="검색어를 입력하세요." value={filteringInfo.search} readOnly/>
+          <input
+            className="search-input"
+            type="text"
+            placeholder="검색어를 입력하세요."
+            value={filteringInfo.search}
+            readOnly
+          />
         </Link>
         <FilterList />
         <StudyRoom />
-        <Link
-          to={isLogin ? '/create-study' : "/login"}
-          className="h-[30px] sm:h-[40px] md:h-[50px] flex items-center justify-center bg-main text-white text-center rounded-[10px]"
-        >
-          <span className="text-sm">{isLogin ? '+ 스터디 만들기' : '로그인'}</span>
-        </Link>
       </div>
+      <Link
+        to={isLogin ? '/create-study' : '/login'}
+        className="bg-main flex h-[30px] items-center justify-center rounded-[10px] text-center text-white sm:h-[40px] md:h-[50px]"
+      >
+        <span className="text-sm">{isLogin ? '+ 스터디 만들기' : '로그인'}</span>
+      </Link>
+    </div>
   );
 }
