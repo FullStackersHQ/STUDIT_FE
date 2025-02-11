8911eb75665fa2ec2ed78a01494168d3335d0cda
choycoy <passionfruits479@gmail.com>
2025-02-11 21:28:18 +0900

    design: 로고 이미지 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom';
-import Logo from '../assets/logo.png';
+import Logo from '../assets/logo.webp';
 import Bell from '@/assets/icons/bell.svg';
 
 export default function Header() {
   return (
     <header className="md:border-white-gray fixed top-0 left-1/2 z-[999] mx-auto flex h-13 w-full max-w-3xl -translate-x-1/2 items-center justify-between bg-white px-4 md:border-x">
       <Link to={'/'} aria-label="홈으로">
-        <img src={Logo} alt="Logo" className="h-[30px] w-20" />
+        <img src={Logo} alt="Logo" className="h-auto w-20" />
       </Link>
       <Bell alt="알림" />
     </header>
