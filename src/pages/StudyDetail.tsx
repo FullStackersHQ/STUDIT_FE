40847810feaa745678fe6c2d3a938ea981d5e4cb
Seungmani <dkelcncjs@naver.com>
2025-02-14 16:48:40 +0900

    [feat] 통계 페이지 생성 및 스터디 세부 페이지 FNB 추가

--- src/pages/StudyDetail.tsx
+++ src/pages/StudyDetail.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HeaderWithBack from '../components/HeaderWithBack';
 import useStudyMain from '../hooks/study-detail/useStudyMain';
 import TodoListSection from '../components/study-detail/TodoListSection';
 import StudyMenuList from '../components/study-detail/StudyMenuList';
+import StudyFnb from '../components/StudyFnb';
+import Statistics from '../components/study-detail/statistics/Statistics';
 
 function StudyDetail() {
+  const [nav, setNav] = useState<string>('스터디');
   const { studyDetail, isLoading, studyId, action, isMenuOpen, userId } = useStudyMain();
   if (!studyDetail || isLoading) return null;
   const { title, leaderId, hasNotice } = studyDetail;
@@ -13,11 +16,17 @@ function StudyDetail() {
   return (
     <div className="relative">
       <HeaderWithBack title={title} isStudyPage action={action} />
-      <div>
-        <div className="bg-main mt-[302px] h-1.5 w-full" />
-        <TodoListSection studyId={studyId} />
-      </div>
-      <StudyMenuList isMenuOpen={isMenuOpen} isLeader={isLeader} studyId={studyId} hasNotice={hasNotice} />
+      {nav === '스터디' && (
+        <>
+          <div>
+            <div className="bg-main mt-[302px] h-1.5 w-full" />
+            <TodoListSection studyId={studyId} />
+          </div>
+          <StudyMenuList isMenuOpen={isMenuOpen} isLeader={isLeader} studyId={studyId} hasNotice={hasNotice} />
+        </>
+      )}
+      {nav === '통계' && <Statistics />}
+      <StudyFnb nav={nav} setNav={setNav} />
     </div>
   );
 }
