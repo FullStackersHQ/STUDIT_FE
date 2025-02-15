8559a4b8a3ee4a48d6671f8c6ee5c3a8e06bb079
Seungmani <dkelcncjs@naver.com>
2025-02-16 01:57:26 +0900

    [feat] 그룹 랭킹 그래프

---
+++ src/components/study-detail/statistics/GroupGraph.tsx
@@ -0,0 +1,90 @@
+import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, Cell, ReferenceLine } from 'recharts';
+import { decimalToTimeString, timeStringToDecimal } from '../../../utils/commonUtils';
+
+interface GroupDataItem {
+  user: string;
+  time: string;
+}
+
+interface GroupDataType {
+  averageTime: string;
+  group: GroupDataItem[];
+}
+
+const groupData: GroupDataType = {
+  averageTime: '19:40',
+  group: [
+    { user: 'User A', time: '15:13' },
+    { user: 'User B', time: '38:59' },
+    { user: 'User C', time: '12:33' },
+    { user: 'My Record', time: '21:41' },
+    { user: 'User D', time: '08:53' },
+  ],
+};
+
+export default function GroupGraph() {
+  const averageTimeDecimal = timeStringToDecimal(groupData.averageTime);
+  const groupStudyRatio: { user: string; time: number; timeLabel: string }[] = groupData.group
+    .map((item: GroupDataItem) => ({
+      user: item.user,
+      time: timeStringToDecimal(item.time),
+      timeLabel: item.time,
+    }))
+    .sort((a, b) => b.time - a.time);
+
+  const renderLabel = ({
+    x,
+    y,
+    value,
+    width,
+    height,
+  }: {
+    x: number;
+    y: number;
+    value: number;
+    width: number;
+    height: number;
+  }) => {
+    const textY = Number(y) + Number(height) / 2;
+    console.log(width, x);
+    return (
+      <text x={70} y={textY} fill={'#fff'} dominantBaseline="central" fontSize={12}>
+        {decimalToTimeString(value)}
+      </text>
+    );
+  };
+  console.log('GroupGraph', groupStudyRatio);
+
+  return (
+    <div className="mb-4">
+      <h2 className="mb-4 text-lg font-semibold">총 공부 시간 랭킹</h2>
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart layout="vertical" data={groupStudyRatio} barSize={20}>
+          <XAxis type="number" domain={[0, 'dataMax']} />
+          <YAxis type="category" dataKey="user" />
+          <Tooltip />
+          <Bar dataKey="time" label={renderLabel}>
+            {groupStudyRatio.map((entry) => (
+              <Cell
+                key={`cell-${entry.user}`}
+                fill={entry.user === 'My Record' ? '#39cccc' : '#616161'} // 'My Record'는 다른 색상
+              />
+            ))}
+          </Bar>
+          <ReferenceLine
+            x={averageTimeDecimal}
+            stroke="#FF6347"
+            strokeDasharray="3 3"
+            isFront
+            label={{
+              value: `평균: ${averageTimeDecimal}`,
+              position: 'insideBottom',
+              fill: '#FF6347',
+              fontWeight: 'bold',
+            }}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
