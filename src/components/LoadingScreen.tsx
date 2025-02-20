6bc9773d9431599f154dfe733096683a0bcfcd6f
choycoy <passionfruits479@gmail.com>
2025-02-20 23:07:02 +0900

    design: 로딩 컴포넌트 배경색 변경 및 크기 증가

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -4,10 +4,10 @@ import Lottie from 'lottie-react';
 
 function LoadingScreen() {
   return (
-    <div className="w-hull bg-white-gray relative h-screen">
+    <div className="w-hull relative h-screen bg-white">
       <Lottie
         animationData={animationData}
-        className="absolute top-1/2 left-1/2 h-[50%] w-[50%] -translate-x-1/2 -translate-y-1/2"
+        className="absolute top-1/2 left-1/2 h-2/3 w-2/3 -translate-x-1/2 -translate-y-1/2"
       />
     </div>
   );
