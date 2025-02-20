0d962e26e3e2299ee0e0da4a61d9da2cd59bd695
choycoy <passionfruits479@gmail.com>
2025-02-20 22:30:37 +0900

    rename: VITE_APP -> VITE 환경 변수 접두사 변경
    VITE_APP_URL ->VITE_SERVER_URL로 환경 변수 이름 변경

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : process.env.VITE_APP_URL;
+const BACKEND_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : process.env.VITE_SERVER_URL;
 
 const client = axios.create({
   baseURL: BACKEND_URL,
