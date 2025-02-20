7ba161e20e2d0ff1bd4ad87775e30cc1d5c24865
choycoy <passionfruits479@gmail.com>
2025-02-20 22:19:12 +0900

    feat: 에러를 서버에 로그로 저장하기 위해 sentry 라이브러리 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import * as Sentry from '@sentry/react';
 
 async function deferRender() {
   const { worker } = await import('./mocks/browser.ts');
@@ -9,7 +10,14 @@ async function deferRender() {
     onUnhandledRequest: 'bypass',
   });
 }
-
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DNS,
+  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ['localhost', import.meta.env.VITE_SERVER_URL],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 deferRender().then(() => {
   createRoot(document.getElementById('root')!).render(<App />);
 });
