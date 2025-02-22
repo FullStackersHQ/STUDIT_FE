e91afd9799efca684ea21cb86ded26900770eb50
choycoy <passionfruits479@gmail.com>
2025-02-22 23:42:19 +0900

    feat: 포인트 페이지 데이터 미존재 시 UI 처리

--- src/components/point/PointHistory.tsx
+++ src/components/point/PointHistory.tsx
@@ -3,6 +3,7 @@ import useGetPointHistory from '../../hooks/point/useGetPointHistory';
 import { toMonthDay } from '../../utils/commonUtils';
 import PointRecordItem from './PointRecordItem';
 import { useIntersectionObserver } from '../../hooks/useIntersectionObserver';
+import emptyPointIcon from '../../assets/imgs/empty-point.png';
 
 export default function PointHistory({ selectedFilter }: { selectedFilter: PointFilterType }) {
   const { pointHistory, isLoading, fetchNextPage, hasNextPage } = useGetPointHistory(selectedFilter);
@@ -13,22 +14,31 @@ export default function PointHistory({ selectedFilter }: { selectedFilter: Point
   if (!pointHistory || isLoading) return null;
 
   return (
-    <ul>
-      {pointHistory.map((pointItem, index) => {
-        const isLastItem = index === pointHistory.length - 1;
-        return (
-          <div key={index} className="mb-1.5">
-            <p className="mb-1.5 text-xs">{toMonthDay(pointItem.date)}</p>
-            <ul className="flex flex-col gap-y-2">
-              {pointItem.records.map((pointRecord: PointRecord) => {
-                return (
-                  <PointRecordItem record={pointRecord} key={pointRecord.id} ref={isLastItem ? setTarget : null} />
-                );
-              })}
-            </ul>
-          </div>
-        );
-      })}
-    </ul>
+    <>
+      {pointHistory ? (
+        <ul>
+          {pointHistory.map((pointItem, index) => {
+            const isLastItem = index === pointHistory.length - 1;
+            return (
+              <div key={index} className="mb-1.5">
+                <p className="mb-1.5 text-xs">{toMonthDay(pointItem.date)}</p>
+                <ul className="flex flex-col gap-y-2">
+                  {pointItem.records.map((pointRecord: PointRecord) => {
+                    return (
+                      <PointRecordItem record={pointRecord} key={pointRecord.id} ref={isLastItem ? setTarget : null} />
+                    );
+                  })}
+                </ul>
+              </div>
+            );
+          })}
+        </ul>
+      ) : (
+        <div className="mt-12 flex h-full items-start justify-center gap-x-2.5">
+          <p className="bg-white-gray mt-2 rounded-full px-2 py-1">{selectedFilter} 포인트 내역이 없어요.</p>
+          <img src={emptyPointIcon} alt="스터딧 캐릭터" className="w-20" />
+        </div>
+      )}
+    </>
   );
 }
