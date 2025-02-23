966165167237deb722f57724ac65a9dc4e48e401
Seungmani <dkelcncjs@naver.com>
2025-02-23 20:50:08 +0900

    [feat] 글이 없는 경우 이미지 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,7 @@ import { pageType } from '../types/interface';
 import FilterTags from '../components/home/FilterTags';
 import RecruitList from '../components/home/RecruitList';
 import OngoingList from '../components/home/OngoingList';
+import emptyImg from '../assets/imgs/empty-study-list.png';
 
 export default function Home({ type = 'recruits' }: { type: pageType }): JSX.Element {
   const { filteringInfo } = useSearchStore();
@@ -37,14 +38,21 @@ export default function Home({ type = 'recruits' }: { type: pageType }): JSX.Ele
           className="scrollbar-hide mt-3 flex w-full flex-col items-center gap-[5px] overflow-y-scroll"
           style={{ height: `${id === -1 ? 'calc(100vh - 200px)' : 'calc(100vh - 250px)'}`, minHeight: '400px' }}
         >
-          {lists.map((list, index) => {
-            const isLastItem = index === lists.length - 1;
-            return type === 'recruits' ? (
-              <RecruitList key={index} info={list} ref={isLastItem ? setTarget : null} />
-            ) : (
-              <OngoingList key={index} info={list} ref={isLastItem ? setTarget : null} />
-            );
-          })}
+          {lists.length === 0 ? (
+            <>
+              <p className="mb-5 text-center">아직 등록된 글이 없어요.</p>
+              <img src={emptyImg} alt={`빈 제목`} className="h-auto w-[76px]" />
+            </>
+          ) : (
+            lists.map((list, index) => {
+              const isLastItem = index === lists.length - 1;
+              return type === 'recruits' ? (
+                <RecruitList key={index} info={list} ref={isLastItem ? setTarget : null} />
+              ) : (
+                <OngoingList key={index} info={list} ref={isLastItem ? setTarget : null} />
+              );
+            })
+          )}
         </div>
       </div>
       {id !== -1 && type === 'recruits' && (
